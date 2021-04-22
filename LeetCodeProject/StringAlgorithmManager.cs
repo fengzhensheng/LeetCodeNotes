628cbe70ee9e30b93199efdd3d73537054984bd4
xiaolezi <1007571685@qq.com>
2021-04-22 23:28:24 +0800

    344. 反转字符串

--- LeetCodeProject/StringAlgorithmManager.cs
+++ LeetCodeProject/StringAlgorithmManager.cs
@@ -33,11 +33,32 @@ namespace LeetCodeProject
             return true;
         }
 
+        // 344. 反转字符串
+        // https://leetcode-cn.com/problems/reverse-string/
+        public static void ReverseString(char[] s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+            while(left < right)
+            {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
         public static void ShowLog()
         {
             //125. 验证回文串
             //string testStr = "A man, a plan, a canal: Panama";
             //Console.WriteLine(IsPalindrome(testStr));
+
+            //344. 反转字符串
+            char[] testChars = new char[]{ 'h','e','l','l','o' };
+            ReverseString(testChars);
+            Console.WriteLine(string.Concat<char>(testChars));
         }
     }
 }
